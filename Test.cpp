fe2f483f3dc8fac6966744b37044ed6520e6e722
yuvili <yuvalsh97@gmail.com>
2023-05-01 15:40:04 +0300

    test

--- Test.cpp
+++ Test.cpp
@@ -7,13 +7,14 @@
 using namespace ariel;
 using namespace std;
 
-TEST_CASE("Fraction object initialization & Getters return expected values") {
+TEST_CASE("Point object initialization & Getters return expected values") {
     Point p(1, 2);
 
     // Check that the type of X and Y are double
     CHECK(typeid(p.getX()).name() == typeid(double).name());
     CHECK(typeid(p.getY()).name() == typeid(double).name());
 
-    // Check that the int constractor's fields are like expected. Also checks the getters
+    // Check that the double constractor's fields are like expected. Also checks the getters
     CHECK(((p.getX() == 1) && (p.getY() == 2)));
 }
+
