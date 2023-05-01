5c03973e55ccabae705a9f0bc287a19d159b315e
yuvili <yuvalsh97@gmail.com>
2023-05-01 15:47:23 +0300

    tests

--- Test.cpp
+++ Test.cpp
@@ -10,12 +10,39 @@ using namespace std;
 TEST_CASE("Point object initialization & Getters return expected values") {
     Point p(1, 2);
 
-    // Check that the type of X and Y are double
-    CHECK(typeid(p.getX()).name() == typeid(double).name());
-    CHECK(typeid(p.getY()).name() == typeid(double).name());
+    SUBCASE("Check that the type of X and Y are double") {
+        CHECK(typeid(p.getX()).name() == typeid(double).name());
+        CHECK(typeid(p.getY()).name() == typeid(double).name());
+    }
 
-    // Check that the double constractor's fields are like expected. Also checks the getters
-    CHECK(((p.getX() == 1) && (p.getY() == 2)));
+    SUBCASE("Check that the double constractor's fields are like expected"){
+        CHECK(((p.getX() == 1) && (p.getY() == 2)));
+    }
+    
+    SUBCASE("Check Point getters"){
+        Point p2(3.14, 2.71);
+
+        CHECK(p2.getX() == doctest::Approx(3.14));
+        CHECK(p2.getY() == doctest::Approx(2.71));
+    }
+}
+
+TEST_CASE("Point moveTowards calculation") {
+    Point p1(0.0, 0.0);
+    Point p2(3.0, 4.0);
+    Point target(5.0, 5.0);
+
+    SUBCASE("Move towards target point") {
+        Point& movedPoint = p1.moveTowards(p2, target, 2.0);
+        CHECK(movedPoint.getX() == doctest::Approx(1.0));
+        CHECK(movedPoint.getY() == doctest::Approx(1.0));
+    }
+
+    SUBCASE("Move towards target point when already at or beyond the target") {
+        Point& movedPoint = p2.moveTowards(p1, target, 2.0);
+        CHECK(movedPoint.getX() == doctest::Approx(3.0));
+        CHECK(movedPoint.getY() == doctest::Approx(4.0));
+    }
 }
 
 TEST_CASE("Point distance calculation") {
